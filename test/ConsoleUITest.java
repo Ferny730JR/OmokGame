90baa280c5b538e5cdefffd93fabc04f9b5ae03a
fcavazos <francisco.cavazos03@gmail.com>
2023-10-23 21:01:37 -0600

    JUnit

---
+++ test/ConsoleUITest.java
@@ -0,0 +1,33 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleUITest {
+    /*
+    InputStream in = new ByteArrayInputStream("6 7\n".getBytes());
+    ByteArrayOutputStream result = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(result, true); // true for auto flushing
+
+    Board board = new Board(15);
+    ConsoleUI ui = new ConsoleUI(board, in, out);
+
+    @BeforeEach
+    void setUp(String testInput) {
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        Board board = new Board(15);
+        ConsoleUI ui = new ConsoleUI(board, in, out);
+    }
+
+    @Test
+    void testSelectGameMode1() {
+        assertEquals("1",ui.selectNextMove(new Player(1,"Player1","X")) );
+    }
+
+     */
+
+}
