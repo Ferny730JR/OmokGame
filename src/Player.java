1621016a2eb16729a9b50c8047147fe66e501361
fcavazos <francisco.cavazos03@gmail.com>
2023-10-24 13:48:53 -0600

    created new Player class

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+/**
+ * A player in an Omok game. It holds the name of the player and
+ * can be used to identify a specific player throughout the game.
+ * The Player class helps to keep track of the moves made by each
+ * player during the game.
+ */
+public class Player {
+
+    /** Name of this player. */
+    private final String name;
+
+    /** Stone of this player. */
+    private final char stone;
+
+    /** Create a new player with the given name. */
+    public Player(String name, char stone) {
+        this.name = name;
+        this.stone = stone;
+    }
+
+    /** Return the name of this player. */
+    public String getName() {
+        return name;
+    }
+
+    /** Return the stone piece of this player. */
+    public char getStone() {
+        return stone;
+    }
+}
