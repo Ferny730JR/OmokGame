97608b3f0d0d535e8762b90037a595053997e5be
fcavazos <francisco.cavazos03@gmail.com>
2023-10-29 13:50:30 -0600

    bug fixes

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
     Board board;
-    List<Player> players = new ArrayList<>();
+    List<Player> players;
 
     // Constructor
     public Game(Board board, List<Player> players) {
@@ -28,7 +27,7 @@ public class Game {
     public boolean moveIsValid(String userInput) {
         int[] move;
         try {
-            move = stringToInt(userInput);
+            move = parseMove(userInput);
         } catch (Exception e) {
             System.out.println("Enter a valid move!");
             return false;
@@ -38,22 +37,21 @@ public class Game {
             System.out.println("Enter a valid move!");
             return false;
         } else {
-            //return board.getBoard()[move[1]-1][move[0]-1] == '•';
-            return true;
+            return board.isEmpty(move[0],move[1]);
         }
     }
 
-    public void makeMove(Computer player, String userInput) {
-        int[] move = stringToInt(userInput);
-        //board.updateBoard(player, move[0], move[1]);
-        board.placeStone(move[1]-1,move[0]-1,player);
+    public void makeMove(Player player, String userInput) {
+        int[] move = parseMove(userInput);
+        board.placeStone(move[0],move[1],player);
     }
 
     // Helper method
-    public int[] stringToInt(String userInput) throws ArrayIndexOutOfBoundsException {
-        String[] move = userInput.split(" ");
-        return new int[]{Integer.parseInt(move[0], board.size()+1),
-                         Integer.parseInt(move[1], board.size()+1)};
+    public int[] parseMove(String userInput) throws ArrayIndexOutOfBoundsException {
+        String splitRegex = "[ ,]";
+        String[] move = userInput.split(splitRegex);
+        return new int[]{Integer.parseInt(move[1], board.size()+1)-1,
+                         Integer.parseInt(move[0], board.size()+1)-1};
     }
 }
 
