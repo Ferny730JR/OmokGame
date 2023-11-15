37f4672ab591b0ce531f97143b31e276ee0f23a1
fcavazos <francisco.cavazos03@gmail.com>
2023-11-15 13:47:44 -0700

    fixed minimax by adding evaluation

--- src/omok/Computer.java
+++ src/omok/Computer.java
@@ -8,6 +8,7 @@ public class Computer extends Player {
     private Player opponent;
     private final int maxDepth;
     private final Random random = new Random();
+    private final HashMap<String, Float> scoreFromPosition = new HashMap<>();
 
     /**
      * Constructor
@@ -15,11 +16,47 @@ public class Computer extends Player {
     public Computer(String name, char playerPiece, int maxDepth) {
         super(name,playerPiece);
         this.maxDepth = maxDepth;
+        initScoreFromPosition();
     }
 
     public Computer(String name, Color color, int maxDepth) {
         super(name, color);
         this.maxDepth = maxDepth;
+        initScoreFromPosition();
+    }
+
+    private void initScoreFromPosition() {
+        // keys: x,y:score. x represents consecutive stones. y represents open ends
+        scoreFromPosition.put("11111",   30000000f);
+        scoreFromPosition.put("22222",  -30000000f);
+        scoreFromPosition.put("011110",  20000000f);
+        scoreFromPosition.put("022220", -20000000f);
+        scoreFromPosition.put("011112",  50000f);
+        scoreFromPosition.put("211110",  50000f);
+        scoreFromPosition.put("022221", -50000f);
+        scoreFromPosition.put("122220", -50000f);
+        scoreFromPosition.put("01110",   30000f);
+        scoreFromPosition.put("02220",  -30000f);
+        scoreFromPosition.put("011010",  15000f);
+        scoreFromPosition.put("010110",  15000f);
+        scoreFromPosition.put("022020",  -15000f);
+        scoreFromPosition.put("020220",  -15000f);
+        scoreFromPosition.put("001112",  2000f);
+        scoreFromPosition.put("211100",  2000f);
+        scoreFromPosition.put("002221",  -2000f);
+        scoreFromPosition.put("122200",  -2000f);
+        scoreFromPosition.put("211010",  2000f);
+        scoreFromPosition.put("210110",  2000f);
+        scoreFromPosition.put("010112",  2000f);
+        scoreFromPosition.put("011012",  2000f);
+        scoreFromPosition.put("122020",  -2000f);
+        scoreFromPosition.put("120220",  -2000f);
+        scoreFromPosition.put("020221",  -2000f);
+        scoreFromPosition.put("022021",  -2000f);
+        scoreFromPosition.put("01100",   500f);
+        scoreFromPosition.put("00110",   500f);
+        scoreFromPosition.put("02200",   -500f);
+        scoreFromPosition.put("00220",   -500f);
     }
 
     public void setOpponent(Player opponent) {
@@ -35,7 +72,7 @@ public class Computer extends Player {
     public int[] makeMove(Board board) {
         if(board.size()>3 && board.getOccupiedPositions().isEmpty()) {
             return new int[]{(board.size()-1)/2, (board.size()-1)/2};
-        } else if(board.size() > 3 && board.getOccupiedPositions().size()==1) {
+        } else if(board.size() > 3 && board.getOccupiedPositions().size()<3) {
             return makeRandomMove(board);
         }
         float bestMoveInfo = miniMax(board.getDeepCopy(), maxDepth);
@@ -56,9 +93,10 @@ public class Computer extends Player {
     }
 
     private float miniMax(Boolean isMaximizingPlayer, Board board, int currentDepth, float alpha, float beta) {
-        if (currentDepth == 0 || board.isFull() || board.isWonBy(this) || board.isWonBy(opponent)) {
-            Player curPlayer = isMaximizingPlayer ? this:opponent;
-            return evalBoard(board, curPlayer);
+        float score = evalBoard(board);
+        if (currentDepth == 0 || board.isFull() || board.isWonBy(this) || board.isWonBy(opponent) || score>=20000000 || score<=-20000000) {
+            //return evalBoard(board);
+            return score;
         }
 
         if (isMaximizingPlayer) {
@@ -100,7 +138,7 @@ public class Computer extends Player {
             alpha = Math.max(alpha,score);
 
             if(beta<=alpha) { // change to <= to improve performance, keep it as < so AI has variety of moves
-                bestScore=1;
+                bestScore=30000000f;
                 break;
             }
 
@@ -108,8 +146,9 @@ public class Computer extends Player {
 
         if(currentDepth == maxDepth) {
             int n = random.nextInt(bestMoves.size());
-            System.out.println(n);
-            //int n = 0;
+            System.out.printf("Best Score: %s, n: %s\n",bestScore, n);
+            for(Board.Place move : bestMoves)
+                System.out.printf("x: %s, y: %s\n",move.x, move.y);
             return bestMoves.get(n).x * board.size() + bestMoves.get(n).y; // returns the best move found
         } else {
             return bestScore;
@@ -138,7 +177,7 @@ public class Computer extends Player {
             beta = Math.min(beta,score);
 
             if(beta<=alpha) { // change to <= to improve performance, keep it as < so AI has variety of moves
-                bestScore=-1;
+                bestScore=-30000000f;
                 break;
             }
         }
@@ -151,7 +190,7 @@ public class Computer extends Player {
      * @param board         The Omok board to play on
      * @return              the evaluation score of the board
      */
-    private float evalBoard(Board board, Player curPlayer) {
+    private float evalBoard(Board board) {
         if (board.size() == 3) { // score for tic-tac-toe board
             if (board.isWonBy(this)) {
                 return 1f;
@@ -162,8 +201,97 @@ public class Computer extends Player {
             }
         }
 
-        // EVALUATION FOR GOMOKU WIP
-        return 0f;
+        // EVALUATION FOR GOMOKU
+        List<String> state = boardToString(board.getBoard());
+
+        float score = 0;
+        for(String boardPos : state) {
+            for(int i=0; i<boardPos.length()-5; i++) {
+                String subState = boardPos.substring(i,i+5);
+                if(scoreFromPosition.containsKey(subState)) {
+                    score += scoreFromPosition.get(subState);
+                }
+            }
+            for(int i=0; i<boardPos.length()-6; i++) {
+                String subState = boardPos.substring(i,i+6);
+                if(scoreFromPosition.containsKey(subState)) {
+                    score += scoreFromPosition.get(subState);
+                }
+            }
+        }
+
+        return score;
+    }
+
+    /**
+     * Return a String representation of the board. The string is represented
+     * using "0" for a null value, "1" for Computer, and "2" for the opponent.
+     *
+     * @param board
+     * @return
+     */
+    private List<String> boardToString(Player[][] board) {
+        List<String> cList = new ArrayList<>(), rList = new ArrayList<>(), dList = new ArrayList<>();
+
+        for(Player[] row : board) {
+            StringBuilder rowVals = new StringBuilder();
+            for(Player player : row) {
+                if(player == this) rowVals.append("1");
+                else if(player == opponent) rowVals.append("2");
+                else rowVals.append("0");
+            }
+            rList.add(rowVals.toString());
+        }
+
+        for(int col=0; col<board.length; col++) {
+            StringBuilder colVals = new StringBuilder();
+            for(int row=0; row<board[0].length; row++) {
+                if(board[row][col] == this) colVals.append("1");
+                else if(board[row][col] == opponent) colVals.append("2");
+                else colVals.append("0");
+            }
+            cList.add(colVals.toString());
+        }
+
+        StringBuilder diagVals;
+        for( int k = 0 ; k < board.length * 2 ; k++ ) {
+            diagVals = new StringBuilder();
+            for( int j = 0 ; j <= k ; j++ ) {
+                int i = k - j;
+                if( i < board.length && j < board.length ) {
+                    if(board[i][j] == this) diagVals.append("1");
+                    else if(board[i][j] == opponent) diagVals.append("2");
+                    else diagVals.append("0");
+                }
+            }
+            dList.add(diagVals.toString());
+        }
+
+        for (int i = board.length - 1; i > 0; i--) {
+            diagVals = new StringBuilder();
+            for (int j = 0, x = i; x <= board.length - 1; j++, x++) {
+                if(board[i][j] == this) diagVals.append("1");
+                else if(board[i][j] == opponent) diagVals.append("2");
+                else diagVals.append("0");
+            }
+            dList.add(diagVals.toString());
+        }
+
+
+        for (int i = 0; i <= board.length - 1; i++) {
+            diagVals = new StringBuilder();
+            for (int j = 0, y = i; y <= board.length - 1; j++, y++) {
+                if(board[j][y] == this) diagVals.append("1");
+                else if(board[j][y] == opponent) diagVals.append("2");
+                else diagVals.append("0");
+            }
+            dList.add(diagVals.toString());
+        }
+        List<String> boardString = new ArrayList<>();
+        boardString.addAll(rList);
+        boardString.addAll(cList);
+        boardString.addAll(dList);
+        return boardString;
     }
 
     /**
